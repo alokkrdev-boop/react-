cae11a17bed5d963e3a6819bfaa3f2c501e1dd49
Alok Kumar <alok.kr.dev@gmail.com>
2025-12-30 23:04:59 +0530

    counter app with useState and styling

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+}
+
+export default App;
