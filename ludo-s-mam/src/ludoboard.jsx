d4c687be12112ab065b6985ec8324d1fa23fe9a2
Alok Kumar <alok.kr.dev@gmail.com>
2026-01-01 22:10:18 +0530

    make ludo counter in react

---
+++ ludo-s-mam/src/ludoboard.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Ludoboard() {
+  const [moves, setMoves] = useState({
+    blue: 0,
+    yellow: 0,
+    green: 0,
+    red: 0,
+  });
+
+  function updateBlue() {
+    setMoves(prev => ({ ...prev, blue: prev.blue + 1 }));
+  }
+
+  function updateYellow() {
+    setMoves(prev => ({ ...prev, yellow: prev.yellow + 1 }));
+  }
+
+  function updateGreen() {
+    setMoves(prev => ({ ...prev, green: prev.green + 1 }));
+  }
+
+  function updateRed() {
+    setMoves(prev => ({ ...prev, red: prev.red + 1 }));
+  }
+
+  return (
+    <div>
+      <p>Game begins</p>
+      <div className="board">
+        <p>Blue moves = {moves.blue}</p>
+        <button style={{ backgroundColor: "blue" }} onClick={updateBlue}>+1</button>
+
+        <p>Yellow moves = {moves.yellow}</p>
+        <button style={{ backgroundColor: "yellow", color: "black" }} onClick={updateYellow}>+1</button>
+
+        <p>Green moves = {moves.green}</p>
+        <button style={{ backgroundColor: "green" }} onClick={updateGreen}>+1</button>
+
+        <p>Red moves = {moves.red}</p>
+        <button style={{ backgroundColor: "red" }} onClick={updateRed}>+1</button>
+      </div>
+    </div>
+  );
+}
+
+export { Ludoboard };
