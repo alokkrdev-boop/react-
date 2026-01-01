f05e2a667a51e4e932ba9a4571403a15875ce472
Alok Kumar <alok.kr.dev@gmail.com>
2026-01-01 12:12:23 +0530

    used dblclicked and onmouseover event in react

--- new react s mam/src/btn.jsx
+++ new react s mam/src/btn.jsx
@@ -2,9 +2,20 @@ function helo () {
     console.log("hello ji")
 }
 
+function pp () {
+    console.log('kya hai bhag yaha se ')
+}
+
+function dbl () {
+    console.log('double clicked ')
+}
 function Btn () {
     return (
+       <>
         <button onClick={helo}>Click ME!</button>
+        <p onMouseOver={pp}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime ab debitis minus temporibus voluptas accusantium voluptates reprehenderit amet? Sequi temporibus non officia adipisci maiores exercitationem voluptatum molestiae quasi nulla!</p>
+        <button onDoubleClick={dbl}>click yahan</button>
+        </>
     )
 }
 export default Btn
