4f0f9e319517b1807fcfd34b8c4d9965c617c2b3
Alok Kumar <alok.kr.dev@gmail.com>
2026-01-07 12:27:31 +0530

    make a form of comment as we see on social media apps or web

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -1,17 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Form from "./form.jsx"
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function CommentsForm() {
+  const [formData, setFormData] = useState({
+    username: "",
+    remarks: "",
+    rating: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormData((currData) => ({
+      ...currData,
+      [name]: value
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // This console output will match your screenshot
+    console.log(formData);
+
+    // Clear form after submit (optional but good practice)
+    setFormData({
+      username: "",
+      remarks: "",
+      rating: ""
+    });
+  }
 
   return (
-    <>
-      <Form/>
-    </>
-  )
-}
+    <div>
+      <h3>Give a comment!</h3>
 
-export default App
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <br />
+        <input
+          type="text"
+          name="username"
+          placeholder="@maggie"
+          value={formData.username}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <label>Remarks</label>
+        <br />
+        <textarea
+          name="remarks"
+          placeholder="great job!"
+          value={formData.remarks}
+          onChange={handleChange}
+        ></textarea>
+
+        <br /><br />
+
+        <label>Rating</label>
+        <br />
+        <input
+          type="number"
+          name="rating"
+          min={1}
+          max={5}
+          value={formData.rating}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">Add Comment</button>
+      </form>
+    </div>
+  );
+}
---
+++ form/src/commentForm.jsx
@@ -0,0 +1,13 @@
+
+export default function Commentform () {
+    return
+    <div>
+        <h4>Give me a form</h4>
+        <form>
+            <input placeholder="userName"  type="text"/>
+            <textarea> Remarks </textarea>
+            <input placeholder="rating" type="number" />
+        </form>
+    </div>
+
+}
